ab40749f60474479dd852dd6411be0e85b397356
Kawish <kawish791@gmail.com>
2025-02-28 02:07:59 +0500

    feat(auth): enhance authentication system and middleware

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,27 @@
 import { env } from "@/env";
+import { oneTapClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  plugins: [
+    oneTapClient({
+      clientId: env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      autoSelect: false,
+      cancelOnTapOutside: true,
+      context: "signin",
+      additionalOptions: {},
+      promptOptions: {
+        baseDelay: 1000,
+        maxAttempts: 5,
+      },
+    }),
+  ],
 });
 
 export const { signIn, signUp, useSession } = authClient;
+
+authClient.$store.listen("$sessionSignal", async () => {
+  const session = await authClient.getSession();
+  console.log(session);
+});
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,12 +3,19 @@ import { env } from "@/env";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
+import { oneTap } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  socialProviders: {
+    google: {
+      clientId: env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
   secret: env.BETTER_AUTH_SECRET,
   baseUrl: env.NEXT_PUBLIC_BETTER_AUTH_URL,
-  plugins: [nextCookies()],
+  plugins: [oneTap(), nextCookies()],
 });
